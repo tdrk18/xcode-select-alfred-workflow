f62839925e57a3cb887e787e1576700c89a6c173
tdrk18 <s18.todoroki@gmail.com>
2020-06-08 00:45:50 +0900

    add method to exec mdfind

--- xcode_list.go
+++ xcode_list.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"os/exec"
 	"strings"
 )
+
+func execMDFind() []string {
+	out, err := exec.Command("mdfind", "-onlyin", "/Applications", "-name", "Xcode").Output()
+	if err != nil {
+		return []string{}
+	}
+	trim := strings.Trim(string(out), "\n")
+	return strings.Split(trim, "\n")
+}
 
 func filterXcodeApp(list []string) []string {
 	var values []string
